7fb97768fa38f1f8948bb36560356e837a704566
JenniferFariasRodrigues <jennifer.farias.rodrigues@gmail.com>
2025-03-20 21:10:30 -0300

    feature: cross validation analisys

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pyspark.sql import SparkSession
 
 load_dotenv()
 
@@ -9,3 +10,23 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME")
 TABLE_NAME = os.getenv("TABLE_NAME")
+
+
+def get_spark_session(app_name="SparkApp"):
+    """
+    Initialize and return a Spark session.
+    """
+    return SparkSession.builder.appName(app_name).getOrCreate()
+
+def load_data_from_mariadb(spark):
+    """
+    Load processed data from MariaDB using JDBC connection.
+    """
+    jdbc_url = f"jdbc:mysql://{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    properties = {
+        "user": DB_USER,
+        "password": DB_PASSWORD,
+        "driver": "com.mysql.cj.jdbc.Driver"
+    }
+    return spark.read.jdbc(url=jdbc_url, table=TABLE_NAME, properties=properties)
+
